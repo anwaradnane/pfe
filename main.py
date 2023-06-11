aac294e84bc9410f2c24c8bebd6313109693d4e8
ANWAR ADNANE <anwaradnane2000@gmail.com>
2023-06-11 01:27:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,79 @@
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
-import chainladder as cl
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
 
-st.markdown("# Hosting actuarial dashboards in Streamlit")
-st.markdown("Here is the top 10 rows from a big rectangle of data. We want to get triangles from this rectangle.")
-n=10
-@st.cache_data(ttl=3600*10)
-def get_data():
-    return pd.read_csv(
-        "https://raw.githubusercontent.com/casact/chainladder-python/master/chainladder/utils/data/clrd.csv"
-    )
-clrd_df = get_data()
-clrd_df_head = clrd_df.head(n)
-clrd_df_head
-st.markdown("""Casualty actuaries create reports with \"loss triangles\". 
-For the triangles we use the [chainladder-python](https://chainladder-python.readthedocs.io/en/latest/intro.html) package. This package is published by the Casualty Actuarial Society and is
-the [most popular open source actuarial project ever](https://www.actuarialopensource.org/).""")
-st.markdown("""We host the loss triangles on the web using streamlit, the [most popular open source python package for hosting dashboards](https://star-history.com/#streamlit/streamlit&plotly/dash&rstudio/shiny&Date).""")
+Data_Scor = {'origine': [2012+i for i in range(10)],
+             '1': [508700, 20100, 342200, 573600, 117700, 156300, 55800, 142600, 314300, 206800],
+             '2': [836800, 433600, 906000, 1159200, 962400, 644700, 404200, 682400, 539700, np.nan],
+             '3': [1094400, 543800, 1391100, 1581800, 1587100, 1172300, 1095500, 1314800, np.nan, np.nan],
+             '4': [1189900, 1073300, 1623400, 2133200, 2226600, 1296400, 1237900, np.nan, np.nan, np.nan],
+             '5': [1358700, 1380400, 1875700, 2352100, 2603300, 1589600, np.nan, np.nan, np.nan, np.nan],
+             '6': [1619300, 1569300, 2224100, 2610400, 2619400, np.nan, np.nan, np.nan, np.nan, np.nan],
+             '7': [1801200, 1559000, 2287600, 2709700, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+             '8': [1865700, 1622800, 2347600, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+             '9': [1866200, 1675700, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+             '10': [1886100, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan]}
 
-clrd = cl.Triangle(
-    clrd_df,
-    origin="AccidentYear",
-    development="DevelopmentYear",
-    columns=[
-        "IncurLoss",
-        "CumPaidLoss",
-        "BulkLoss",
-        "EarnedPremDIR",
-        "EarnedPremCeded",
-        "EarnedPremNet",
-    ],
-    index="LOB",
-    cumulative=True,
-)
+# Manipulation des Donnees:
+Triangle = pd.DataFrame(Data_Scor)
+Triangle.set_index(Triangle['origine'], inplace=True)
+del Triangle['origine']
 
-st.write("Selectors")
+# Affichage du Triangle:
+st.write("Triangle:")
+st.write(Triangle)
 
-lob = st.selectbox("Line of Business", clrd.index)
+# Plot du Triangle:
+st.write("Plot du Triangle:")
+fig, ax = plt.subplots(figsize=(12, 6))
+Triangle.plot(ax=ax)
+st.pyplot(fig)
 
-column = st.selectbox("Type", clrd.columns)
+# Utilisation de Chain_Ladder:
+facteurs = []
+for col in Triangle.columns[:-1]:
+    facteurs.append(Triangle[str(int(col) + 1)].sum() / Triangle[col][:-int(col)].sum())
+facteurs = np.array(facteurs)
 
-triangle = clrd.loc[lob, column]
+# Affichage des Facteurs:
+st.write("Facteurs:")
+st.line_chart(pd.DataFrame({'Periode du Developpement': range(1, 10), 'Facteurs': facteurs}))
 
-st.markdown("This is a triangle for " + lob + " and " + column + ".")
+# Fitting du Model:
+model = LinearRegression()
+model.fit(np.arange(1, 10).reshape(-1, 1), np.log(facteurs - 1))
+delta = np.exp(model.intercept_ + model.coef_ * np.arange(10, 111)) + 1
+st.write("Fitting du Model - Delta:")
+st.write(delta)
+st.write("Produit de Delta:")
+st.write(delta.prod())
 
-triangle
+# Compléter le Triangle:
+for i, col in enumerate(Triangle.columns[1:]):
+    for j in range(i + 1):
+        Triangle[col].at[2021 - j] = facteurs[i] * Triangle[str(int(col) - 1)].at[2021 - j]
 
-st.markdown("This is a heatmap of the link ratio.")
+# Affichage du Triangle Complet:
+st.write("Triangle Complet:")
+st.write(Triangle)
 
-st.write(triangle.link_ratio.heatmap())
+# Plot du Triangle Complet:
+st.write("Plot du Triangle Complet:")
+fig, ax = plt.subplots(figsize=(12, 6))
+Triangle.T.plot(ax=ax)
+st.pyplot(fig)
+
+# Calcul de l'ultim et de l'IBNR:
+Triangle['ultim'] = Triangle['10'] * delta.prod()
+Triangle['IBNR'] = Triangle['ultim'].subtract(Triangle['10'])
+
+# Affichage du Triangle avec ultim et IBNR:
+st.write("Triangle avec ultim et IBNR:")
+st.write(Triangle)
+
+# Affichage du total:
+st.write("Total:")
+st.write(Triangle.sum())
